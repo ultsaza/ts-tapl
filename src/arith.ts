b35d42007e93102d6f3b8b834e8bc0f590c56dc5
kyomu <132206022+ultsaza@users.noreply.github.com>
2025-05-14 01:54:10 +0900

    implement typecheck function in arith.ts for term validation

--- src/arith.ts
+++ src/arith.ts
@@ -11,3 +11,20 @@ type Term =
     | { tag: "number"; n: number }
     | { tag: "add", left: Term, right: Term }
 
+function typecheck(t: Term): Type {
+    switch (t.tag) {
+        case "true":
+            return { tag: "Boolean" };
+        case "false":
+            return { tag: "Boolean" };
+        case "if": {
+
+        }
+        case "number": {
+            return { tag: "Number" };
+        }
+        case "add": {
+
+        }    
+    }
+}
