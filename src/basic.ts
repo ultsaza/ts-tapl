7079539700cd5ee8f826069afd1762d6a0272cde
kyomu <132206022+ultsaza@users.noreply.github.com>
2025-05-14 02:55:29 +0900

    add: implement typeEq function for type comparison in basic.ts

--- src/basic.ts
+++ src/basic.ts
@@ -45,5 +45,19 @@ function typecheck(t: Term): Type {
   }
 }
 
-const node = parseBasic("(f: (x: number) => number) => 1");
-console.log(node);
+function typeEq(ty1: Type, ty2: Type): boolean {
+    switch (ty2.tag) {
+        case "Boolean":
+            return ty1.tag === "Boolean";
+        case "Number":
+            return ty1.tag === "Number";
+        case "Func": {
+            if (ty1.tag !== "Func") return false;
+            if (ty1.params.length !== ty2.params.length) return false;
+            for (let i = 0; i < ty1.params.length; i++) {
+                if (!typeEq(ty1.params[i].type, ty2.params[i].type)) return false;
+            }
+            return typeEq(ty1.retType, ty2.retType);
+        }
+    }
+}
